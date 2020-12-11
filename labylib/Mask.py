c8fe34620d14250c81c10bf0665da8f3c6923a88
Victor Westerlund <victor.vesterlund@gmail.com>
2020-12-11 14:02:56 +0100

    Added new cosmetic "Mask"
    Cosmetics with values of type="multi" now have a single class "Multi" where each value is set by calling a function. LabyMod expects all mutli values to be set in order to update the cosmetic. Labylib does not pull current values as of yet.
    
    Added "Mask" cosmetic.

--- labylib/Mask.py
+++ labylib/Mask.py
@@ -28,7 +28,7 @@ class Visibility:
 
 		# Payload
 		self.addEncodedFormData("type","switch")
-		self.addEncodedFormData("item",525800)
+		self.addEncodedFormData("item",544349)
 		self.addEncodedFormData("site","control")
 
 	# -----------------------------------
@@ -91,11 +91,11 @@ class Multi:
 		}
 
 		self.body = ""
-		self.values = (000000,000000)
+		self.values = (000000,000000,0,0) # Color,Color,Size,Texture
 
 		# Payload
 		self.addEncodedFormData("type","multi")
-		self.addEncodedFormData("item",525800)
+		self.addEncodedFormData("item",544349)
 		self.addEncodedFormData("site","control")
 
 	# -----------------------------------
@@ -108,6 +108,39 @@ class Multi:
 		
 		self.values[0] = color[0]
 		self.values[1] = color[1]
+
+	def size(size):
+		# Interpret strings 
+		if(type(size) != int):
+			if(size == "big"):
+				size = 1
+			else:
+				size = 0
+
+		self.values[2] = size
+
+	def template(value):
+		templates = {
+			"None": "null",
+			"3": "26b459ce-2f09-4b50-97ad-c589262b0268",
+			":3": "3b7492c6-4239-4e4b-9906-d510e722a96b",
+			":*)": "4120f1eb-19e6-4f34-8417-d7bd6a01d85a",
+			":#)": "43e1b6bf-2a18-40f4-b0b0-38b7c74a6ddb",
+			":'3": "4b498414-5894-4124-98f6-b6c5a531b6e8",
+			":)": "84456b05-85ba-48bf-a0c8-ca4ba981ff8e",
+			":p)": "a387dde9-a1a8-41b8-9e77-8446f8d6d3cd",
+			":P": "aaa3db95-6537-4ef0-aa66-050b20412ef6",
+			":')": "bb9006f9-b67c-4fff-996b-6d74a1b691d6",
+			"::)": "c6aa01db-bd10-4948-9632-afe6bd0c3f3c",
+			"X": "e93ec7b5-4148-4d1c-aefd-26c3f125cec7",
+			";p": "ed2c847c-5b2b-4916-843d-438b6c039804",
+			":~)": "f9e4f616-e697-441e-be57-1b64cc5c5e41"
+		}
+
+		if(value not in templates):
+			raise ValueError(f"'{value}' is not a valid template.")
+			
+		self.values[3] = templates[value]
 
 	# -----------------------------------
 
